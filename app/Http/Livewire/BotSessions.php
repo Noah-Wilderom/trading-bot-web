b7fc823fec29f7aee9f22deff95cf60d6d95ceb5
nwilderom <nwilderom@flaire.nl>
2022-07-27 16:06:40 +0200

    ssh fix

--- app/Http/Livewire/BotSessions.php
+++ app/Http/Livewire/BotSessions.php
@@ -20,6 +20,15 @@ class BotSessions extends Component
     public $newCoin;
     public $newName;
 
+    private $ssh;
+
+    public function boot()
+    {
+        $this->ssh = Ssh::create('root', '5.181.134.239', 22)
+            ->usePrivateKey(__DIR__ . '/ssh/id_rsa')
+            ->disablePasswordAuthentication();
+    }
+
     public function createSession()
     {
         $this->createSessionModal = true;
@@ -54,15 +63,12 @@ class BotSessions extends Component
     public function downloadLogs()
     {
         toastr()->addSuccess("Logs are being downloaded please wait...");
-        $ssh = Ssh::create('root', '5.181.134.239', 22)
-            ->usePrivateKey(__DIR__ . '/ssh/id_rsa')
-            ->disablePasswordAuthentication();
 
         $dir = Storage::makeDirectory(Auth::user()->email);
 
         if($dir)
         {
-            $logs = $ssh->download('/home/noahdev/tradingbot/logs/*.log', Storage::path(Auth::user()->email));
+            $logs = $this->ssh->download('/home/noahdev/tradingbot/logs/*.log', Storage::path(Auth::user()->email));
         }
 
         foreach(Storage::files($dir) as $file)
@@ -87,13 +93,9 @@ class BotSessions extends Component
     public function render()
     {
         $this->botSessions = Auth::user()->botSessions;
-        $ssh = Ssh::create('root', '5.181.134.239', 22)
-            ->usePrivateKey(__DIR__ . '/ssh/id_rsa')
-            ->disablePasswordAuthentication();
-
-        $check = $ssh->execute('whoami')->isSuccessful();
-        $coins = $ssh->execute(['cd /home/noahdev/tradingbot', 'python3 main.py --coins'])->getOutput();
-        $version = $ssh->execute(['cd /home/noahdev/tradingbot', 'python3 main.py --version'])->getOutput();
+        $check = $this->ssh->execute('whoami')->isSuccessful();
+        $coins = $this->ssh->execute(['cd /home/noahdev/tradingbot', 'python3 main.py --coins'])->getOutput();
+        $version = $this->ssh->execute(['cd /home/noahdev/tradingbot', 'python3 main.py --version'])->getOutput();
                 
         return view('livewire.bot-sessions', compact('check', 'version', 'coins'));
     }
