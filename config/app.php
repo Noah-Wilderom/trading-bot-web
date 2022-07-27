8d5374dd55237b1e3bb90d126260423c5322f061
Noah-Wilderom <wilderomnoah@gmail.com>
2022-07-27 23:27:22 +0200

    added debugbar

--- config/app.php
+++ config/app.php
@@ -197,6 +197,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Flasher\Laravel\FlasherServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
