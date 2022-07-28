b5ac4607e605dc021459311e34a85dd31683c033
Noah-Wilderom <wilderomnoah@gmail.com>
2022-07-28 14:08:48 +0200

    update

--- resources/views/livewire/bot-logs.blade.php
+++ resources/views/livewire/bot-logs.blade.php
@@ -1,7 +1,7 @@
 <div>
     @foreach($logs as $log)
         <div>
-            [{{ $log->created_at->format('H:m:s | m-d-Y') }}] {{ $log->log }}
+            {{ $log->log }}
         </div>
     @endforeach
 </div>
